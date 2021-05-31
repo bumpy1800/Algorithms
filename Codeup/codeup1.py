1f9c0f6ef47b54782b9837a3aa744b1bb329da98
bumpy1800 <bumpy1800@gmail.com>
2021-06-01 01:43:28 +0900

    codeup(python)1-30

---
+++ Codeup/codeup1.py
@@ -0,0 +1,132 @@
+#코드업 파이썬 기초문제 1
+print("Hello")
+
+#코드업 파이썬 기초문제 2
+print("Hello World")
+
+#코드업 파이썬 기초문제 3
+print("Hello\nWorld")
+
+#코드업 파이썬 기초문제 4
+print("\'Hello\'")
+
+#코드업 파이썬 기초문제 5
+print("\"Hello World\"")
+
+#코드업 파이썬 기초문제 6
+print("\"!@#$%^&*()\'")
+
+#코드업 파이썬 기초문제 7
+print("\"C:\\Download\\\'hello\'.py\"")
+
+#코드업 파이썬 기초문제 8
+print("print(\"Hello\\nWorld\")")
+
+#코드업 파이썬 기초문제 9
+x=input()
+print(x)
+
+#코드업 파이썬 기초문제 10
+x=input()
+x=int(x)
+print(x)
+
+#코드업 파이썬 기초문제 11
+x=input()
+x=float(x)
+print(x)
+
+#코드업 파이썬 기초문제 12
+a=input()
+b=input()
+print(a)
+print(b)
+
+#코드업 파이썬 기초문제 13
+a=input()
+b=input()
+print(b)
+print(a)
+
+#코드업 파이썬 기초문제 14
+a=input()
+a=float(a)
+print(a)
+print(a)
+print(a)
+
+#코드업 파이썬 기초문제 15
+a,b=input().split()
+print(a)
+print(b)
+
+#코드업 파이썬 기초문제 16
+a,b=input().split()
+print(b,a)
+
+#코드업 파이썬 기초문제 17
+a=input()
+print(a,a,a)
+
+#코드업 파이썬 기초문제 18
+a,b=input().split(':')
+print(a,b, sep=':')
+
+#코드업 파이썬 기초문제 19
+y,m,d=input().split('.')
+print(d,m,y, sep='-')
+
+#코드업 파이썬 기초문제 20
+a,b=input().split('-')
+print(a,b,sep='')
+
+#코드업 파이썬 기초문제 21
+a=input()
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+
+#코드업 파이썬 기초문제 22
+x=input()
+print(x[0:2],x[2:4],x[4:6])
+
+#코드업 파이썬 기초문제 23
+time=input().split(':')
+print(time[1])
+
+#코드업 파이썬 기초문제 24
+a,b=input().split()
+x= a+b
+print(x)
+
+#코드업 파이썬 기초문제 25
+a,b=input().split()
+x= int(a)+int(b)
+print(x)
+
+#코드업 파이썬 기초문제 26
+a=input()
+b=input()
+x= float(a)+float(b)
+print(x)
+
+#코드업 파이썬 기초문제 27
+a=input()
+a=int(a)
+print('%x'%a)
+
+#코드업 파이썬 기초문제 28
+a=input()
+a=int(a)
+print('%X'%a)
+
+#코드업 파이썬 기초문제 29
+a=input()
+a=int(a,16)
+print('%o'%a)
+
+#코드업 파이썬 기초문제 30
+a=ord(input())
+print(a)
