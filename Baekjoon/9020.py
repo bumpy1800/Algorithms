bc9123879ef22e2365f31710bb0534e1ee471e77
bumpy1800 <bumpy1800@gmail.com>
2021-06-17 22:23:22 +0900

    baekjoon 9020(python) 수정보완

--- Baekjoon/9020.py
+++ Baekjoon/9020.py
@@ -1,23 +1,27 @@
 # 9020번 문제 : 골드바흐의 추측
 # 골트바흐의 추측은 2보다 큰 짝수는 두 소수의 합으로 나타낼 수 있다는 것이며, 주어진 수를 어떤 소수로 합하면 나오는지 출력하는 문제
 
-prime_list = [False, False] + [True]*10002
+sosu = [False, False] + [True]*10002 # 에라토스테네스의 체를 통해 최대 수이하의 소수들을 구한다
 
 for i in range(2, 10002):
-    if prime_list[i]:
+    if sosu[i]:
         for j in range(2*i, 10002, i):
-            prime_list[j] = False
+            sosu[j] = False
 
-T = int(input())
-
-for i in range(T):
-    n = int(input())
-    a = n//2
-    b = a
-    while a>0:
-        if prime_list[a] and prime_list[b]:
-            print(a, b)
-            break
+def sum(m): # 주어진 수의 2분의1 부터 간격을 넓혀 가면서 소수이면서 더했을 시 주어진 수와 같아지는 수를 출력
+    x=m//2
+    y=x
+    while m>0:
+        if((sosu[x] and sosu[y]) and (x+y==m)):
+            return x,y
         else:
-            a-=1
-            b+=1
+            x-=1
+            y+=1
+
+
+num = int(input())
+
+for i in range(num): # 출력
+    n=int(input())
+    x,y=sum(n)
+    print(x,y)
